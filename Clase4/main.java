7afce43252de29dafdf6a4562989881f93504723
JorgePerC <42452493+JorgePerC@users.noreply.github.com>
2020-06-12 18:31:28 -0500

    Added comentarios

--- Clase4/main.java
+++ Clase4/main.java
@@ -5,29 +5,32 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        // System.out.println ("Esbribe el mensaje");
-        // String dictadoTexto=teclado.next();
+        
+        //Santi
 
-        // Santi pluma = new Santi ("Bic", true, true);
-        // pluma.escribir(dictadoTexto);
+        System.out.println ("Esbribe el mensaje");
+        String dictadoTexto=teclado.next();
+
+        Santi pluma = new Santi ("Bic", true, true);
+        pluma.escribir(dictadoTexto);
 
 
         //----RAFA
-        // System.out.println("Gorrros en el mundo: es cero") ;
+        System.out.println("Gorrros en el mundo: es cero") ;
 
-        // Hat hat1 = new Hat(5, "round", "blue");
-        // System.out.println("Gorrros en el mundo: " + hat1.getHatsInTheWorld());
-        // Hat hat2 = new Hat(10, "round", "red");   
-        // System.out.println("Gorrros en el mundo: " + hat1.getHatsInTheWorld());
-        // hat1.wearHat();
+        Hat hat1 = new Hat(5, "round", "blue");
+        System.out.println("Gorrros en el mundo: " + hat1.getHatsInTheWorld());
+        Hat hat2 = new Hat(10, "round", "red");   
+        System.out.println("Gorrros en el mundo: " + hat1.getHatsInTheWorld());
+        hat1.wearHat();
 
-        // hat2.throwHat();
+        hat2.throwHat();
 
-        // hat1.makeSize(10);
+        hat1.makeSize(10);
 
         //--- Sofi
-        // Cereal cereal = new Cereal("Nestle", "azul", 20);
-        // cereal.escogerCereal();
+        Cereal cereal = new Cereal("Nestle", "azul", 20);
+        cereal.escogerCereal();
 
         //Nuestro array está vacio
         String [] arrayDeStrings = new String [10];
